84242bd7d35f6bc07281e35c4eb24d43bd7ff74b
Dhiren Rajesh <81640330+dhiren-rajesh@users.noreply.github.com>
2021-07-08 12:00:59 +0530

    Added Final Frontend Files

--- src/Navbar.js
+++ src/Navbar.js
@@ -16,7 +16,7 @@ export default function Navbar() {
         <div className="row" id="about">
             <div className="feature-box">
                 <h3 className="feature-title">Easy to use</h3>
-                <p   className="content">Just have to Upload and Analyze</p>
+                <p  className="content">Just have to Upload or Drag and Drop an Image</p>
             </div>
             <div className="feature-box">
                 <h3 className="feature-title">Quick and Responsive</h3>
